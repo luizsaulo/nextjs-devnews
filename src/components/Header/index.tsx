22d208ec2b4a5b20ea6e49726fbd60faa83cb175
Luiz <luizsaulo@yahoo.com.br>
2022-04-05 16:24:22 -0300

    adding the header component and the active link stylization

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,22 @@
 import styles from './styles.module.scss';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export function Header() {
+    const { asPath } = useRouter();
+
+    
     return (
         <header className={styles.container}>
             <div className={styles.content}>
                 <img src="/logo.svg" alt="DevNews!" />
                 <nav>
-                    <a href="#">Home</a>
-                    <a href="#">Posts</a>
+                    <Link href="/">
+                        <a className={asPath === '/' ? styles.active : ' '}>Home</a>                    
+                    </Link>
+                    <Link href="/posts">
+                        <a className={asPath === '/posts' ? styles.active : ' '}>Posts</a>                    
+                    </Link>
                 </nav>
             </div>
         </header>
