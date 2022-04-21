3942be9c165a3edbd3d90208514bc1ffe051e6c3
Luiz <luizsaulo@yahoo.com.br>
2022-04-21 17:08:59 -0300

    consuming the prismic api

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,6 +1,9 @@
 import { GetStaticProps } from "next";
+import Prismic from "@prismicio/client";
 import Link from "next/link";
 import SEO from "../../src/components/SEO";
+import { getPrismicClient } from "../../src/components/services/prismic";
+import styles from './posts.module.scss';
 
 interface Post {
   id: string;
@@ -15,8 +18,8 @@ export default function Posts() {
     return (
       <>
         <SEO title="Posts" />
-        <main>
-          <div>
+        <main className={styles.container}>
+          <div className={styles.posts}>
             <Link href="#">
               <a>
                 <time>20 de abril de 2022</time>
@@ -30,11 +33,20 @@ export default function Posts() {
     );
   }
   
-  export const getStaticProps: GetStaticProps = async () => {
-    
+  export const getStaticProps: GetStaticProps = async () => {  
+    const prismic = getPrismicClient();
+
+    const response = await prismic.query(
+      [Prismic.predicates.at('document.type', 'post')], 
+      {
+      fetch: ['post.title', 'post.content'],
+      },
+    );
+
+      console.log(response);
 
     return {
       props: {},
-      revalidate: 60 * 60 * 12, //in seconds
+      revalidate: 60 * 60 * 12, //12 horas
     };
   };
