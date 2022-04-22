37b96473598c0f1297d95be4d612bf69108e670b
Luiz <luizsaulo@yahoo.com.br>
2022-04-22 09:01:17 -0300

    continuing with the api consumption

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,32 +1,37 @@
 import { GetStaticProps } from "next";
 import Prismic from "@prismicio/client";
+import { RichText } from 'prismic-dom';
 import Link from "next/link";
 import SEO from "../../src/components/SEO";
 import { getPrismicClient } from "../../src/components/services/prismic";
 import styles from './posts.module.scss';
 
 interface Post {
-  id: string;
+  slug: string;
   title: string;
+  excerpt: string;
+  updateAt: string;
 }
 
 interface PostsProps {
   posts: Post[];
 }
 
-export default function Posts() {
+export default function Posts({ posts }: PostsProps) {
     return (
       <>
         <SEO title="Posts" />
         <main className={styles.container}>
           <div className={styles.posts}>
-            <Link href="#">
-              <a>
-                <time>20 de abril de 2022</time>
-                <strong>Título</strong>
-                <p>Parágrafo</p>
-              </a>
-            </Link>
+            {posts.map(post => (
+              <Link href="#" key={post.slug}>
+                <a>
+                  <time>{post.updateAt}</time>
+                  <strong>{post.title}</strong>
+                  <p>{post.excerpt}</p>
+                </a>
+              </Link>
+            ))}
           </div>
         </main>      
       </>
@@ -43,10 +48,27 @@ export default function Posts() {
       },
     );
 
-      console.log(response);
+      const posts = response.results.map(post => {
+        return {
+          slug: post.uid,
+          title: RichText.asText(post.data.title),
+          excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? 
+          '',
+          updateAt: new Date(post.last_publication_date).toLocaleDateString(
+            'pt-BR', 
+            {
+              day: '2-digit',
+              month: 'long',
+              year: 'numeric',
+            },
+          ),
+        };
+      });
 
     return {
-      props: {},
+      props: {
+        posts,
+      },
       revalidate: 60 * 60 * 12, //12 horas
     };
   };
