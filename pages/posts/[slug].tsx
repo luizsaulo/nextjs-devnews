95bcd9f21b0e050dd425b4e3c63e458541f56414
Luiz <luizsaulo@yahoo.com.br>
2022-04-28 08:39:42 -0300

    correcting the typage slug page on posts folder in order to be able to deploy the app

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,11 +1,10 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
-import Prismic from "@prismicio/client";
 import { RichText } from 'prismic-dom';
 import { getPrismicClient } from "../../src/components/services/prismic";
 import SEO from "../../src/components/SEO";
 import styles from './post.module.scss';
-
+ 
 interface PostProps {
   post: {
     slug: string;
@@ -14,18 +13,34 @@ interface PostProps {
     updatedAt: string;
   }
 }
-
+ 
+interface PrismicDocumentTitle {
+  type: string;
+  text: string;
+}
+ 
+interface PrismicDocumentContent {
+  type: string;
+  text: string;
+}
+ 
+interface PrismicDocument {
+  uid: string;
+  last_publication_date: string;
+  data: {
+    title: PrismicDocumentTitle[];
+    content: PrismicDocumentContent[];
+  }
+}
+ 
 export default function Post({ post }: PostProps) {
   const router = useRouter();
-
   if (router.isFallback) {
     return <p>Loading...</p>
   }
-
   return (
     <>
        <SEO title="Post" />
-
         <main className={styles.container}>
           <article className={styles.post}>
             <h1>{post.title}</h1>
@@ -36,35 +51,32 @@ export default function Post({ post }: PostProps) {
     </>
   );
 }
-
+ 
 export const getStaticPaths: GetStaticPaths = async () => {  
   return {
     paths: [],
     fallback: true,
   };
 };
-
+ 
 export const getStaticProps: GetStaticProps = async context => {
   const {slug} = context.params;
-  
   const prismic = getPrismicClient();
-
-    const response = await prismic.getByUID('post', String(slug), {})
-
-      const post = {
-          slug,
-          title: RichText.asText(response.data.title),
-          content: RichText.asHtml(response.data.content),
-          updateAt: new Date(response.last_publication_date).toLocaleDateString(
-            'pt-BR', 
-            {
-              day: '2-digit',
-              month: 'long',
-              year: 'numeric',
-            },
-          ),
-        };
-      
+  const response = await prismic.getByUID('post', String(slug), {})
+  const document = response as PrismicDocument;
+  const post = {
+    slug,
+    title: RichText.asText(document.data.title),
+    content: RichText.asHtml(document.data.content),
+    updateAt: new Date(document.last_publication_date).toLocaleDateString(
+      'pt-BR', 
+      {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      },
+    ),
+  };
   return {
     props: {
       post,
@@ -72,4 +84,3 @@ export const getStaticProps: GetStaticProps = async context => {
     revalidate: 60 * 60 * 12, //12 horas
   };
 };
-
